17d938ba263bc9708234ce48bcbeecf9f89a0cb0
goldensniper514 <mountiebalar@gmail.com>
2025-04-15 19:26:20 -0400

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    GITHUB_TOKEN: "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"
-  }
+  
   /* config options here */
 };
 
